8c21af5f32f37f07ccdba7563b860f4949f0f84f
Rony_Allaw <rga45@mail.aub.edu>
2023-12-16 23:38:10 +0200

    Rony Entry Commit

--- src/main/java/com/democrapp/vote/VoteApplication.java
+++ src/main/java/com/democrapp/vote/VoteApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VoteApplication {
-
+	// bogus comment
 	public static void main(String[] args) {
 		SpringApplication.run(VoteApplication.class, args);
 	}
